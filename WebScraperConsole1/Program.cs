28fb9b8a8ac810f5c7f04871cf52e2eb149bca38
Usman Idris Kwaru <kwaru91@gmail.com>
2021-11-30 13:24:49 +0100

    Add project files.

---
+++ WebScraperConsole1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+
+/// <summary>
+/// A webscraping application that gets information from multiple webpages with the same signature
+/// Incremente with numbers
+/// </summary>
+namespace WebScraperConsole1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            String m = "";
+            String err = "";
+            int counter = 281; //The number of webpage to start
+            string url = "https://boxnovel.com/novel/chronicles-of-primordial-wars/chapter-"; //The signature url
+            HtmlWeb web = new HtmlWeb();
+
+            try
+            {
+                while (counter <= 846)
+                {
+                    HtmlDocument doc = web.Load(url + counter);
+
+                    var HeaderNames = doc.DocumentNode.SelectNodes("//div[@class='text-left']").ToList();
+                    foreach (var item in HeaderNames)
+                    {
+                        m += item.InnerText;
+                        Console.WriteLine(item.InnerText);
+                    }
+
+                    counter++;
+                }
+            }
+            catch (Exception e)
+            {
+                err = e.Message;
+            }
+            finally
+            {
+                TextCreate txt = new TextCreate(m);
+                txt.CreateFile();
+                Console.WriteLine(err);
+            }
+        }
+    }
+}
---
+++ WebScraperConsole1/TextCreate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WebScraperConsole1
+{
+    class TextCreate
+    {
+        readonly String s;
+        public TextCreate(String s)
+        {
+            this.s = s;
+        }
+
+        //public static async Task ExampleAsync()
+        //{
+        //    using(StreamWriter file = new StreamWriter("WriteLines2.txt", append: true))
+        //    await file.WriteLineAsync("Fourth line");
+        //}
+
+        public void CreateFile()
+        {
+            //File and path you want to create and write to
+            string fileName = @"C:\Users\USER\Documents\Temp.txt";
+            //Check if the file exists
+            if (!File.Exists(fileName))
+            {
+                // Create the file and use streamWriter to write text to it.
+                //If the file existence is not check, this will overwrite said file.
+                //Use the using block so the file can close and vairable disposed correctly
+                using (StreamWriter writer = File.CreateText(fileName))
+                {
+                    writer.WriteLine(s);
+                }
+            }
+        }
+
+        
+    }
+}
